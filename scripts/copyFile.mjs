f5807572f5fcc863cfd7dc32b11121f233e3812c
duan602728596 <duanhaochen@126.com>
2025-01-20 14:58:46 +0800

    build: 拷贝python代码和前端资源文件

---
+++ scripts/copyFile.mjs
@@ -0,0 +1,13 @@
+import path from 'node:path';
+import { copy } from 'fs-extra/esm';
+import { metaHelper } from '@sweet-milktea/utils';
+
+const { __dirname } = metaHelper(import.meta.url);
+
+/* 拷贝python代码和前端资源文件 */
+const vercelBuildOutput = path.join(__dirname, '../dist2');
+
+await Promise.all([
+  copy(path.join(__dirname, '../src'), path.join(vercelBuildOutput, 'api')),
+  copy(path.join(__dirname, '../dist'), path.join(vercelBuildOutput, 'dist'))
+]);
